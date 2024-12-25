5f4c2cfebdbeb36b731005085c1e619df2b40728
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2024-12-25 15:37:08 +0530

    new project added

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='text-red-500 text-5xl'>React Project</p>
+    </div>
+  );
+}
+
+export default App;
