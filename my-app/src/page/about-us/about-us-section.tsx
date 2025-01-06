aaf376e9c2c81fa8966747916a6943dcb1a616df
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2025-01-06 13:16:54 +0530

    content added

--- my-app/src/page/about-us/about-us-section.tsx
+++ my-app/src/page/about-us/about-us-section.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CustomUnderline } from '../../components/layouts/custom-underline';
 import { EnquireForm } from '../../components/enquire-form.tsx';
+import { WhyUsText } from '../../components/layouts/why-us-text';
 
 const AboutUsSection: React.FC = () => {
     return <div className='w-full min-h-[500px] lg:flex'>
@@ -9,17 +10,22 @@ const AboutUsSection: React.FC = () => {
                 <p className='mt-2 text-3xl md:text-5xl font-[1000] text-meta-orange'>ABOUT US</p>
             </CustomUnderline>
             <div className='md:w-[65%] mt-5'>
-                <p>
-                    1885 Delivery Services, a company invested by Easy Lease Motorcycle Rental, is a subsidiary of International Holding Company (IHC), Abu Dhabi,
-                    providing last and middle mile services across UAE.
+                <p>Hornokplease best Packing And Moving Companie in Dubai has been designed to provide you with the best experience of
+                    Packing And Moving Dubai, Rent a truck, and delivery service. We are providing hassle-free movement & the best solution for
+                    your logistics handling. Our goal is to be UAE’S best Dealer of “Rent a truck”, Movers in Dubai, and provide end-to-end logistics
+                    platforms and revolutionize the transport sector for our customers. You can find us on dubai app sore, we are the best packers
+                    and movers in UAE.
                 </p>
-                <br />
-                <p>The company was formed with a vision to capture substantial market share with a customer-centric approach and provide an end-to-end delivery solution.</p>
-                <br />
-                <p>
-                    We go the extra mile when providing delivery-related solutions to get your logistics where they need to go. Our team is committed to serving its clients the best logistics servicesin the last and middle mile delivery space.</p>
-                <br /><p>We are the trusted last mile delivery solution for reputed companies in the UAE across dozens of industries, including food and beverage, retail, e-commerce, furniture, healthcare centres, pharmacy and more.</p>
-                <p>At 1885 Delivery Services, we don’t just promise, but we provide a seamless delivery solution.</p>
+                <div className="mt-8 px-4 lg:px-0">
+                    <WhyUsText title={'01'} text={'Dedicated and Professional'} />
+                    <WhyUsText title={'01'} text={'Moving Protection'} />
+                    <WhyUsText title={'01'} text={'Commercial Moves'} />
+                    <WhyUsText title={'01'} text={'Every Day Delivery'} />
+                    <WhyUsText title={'01'} text={'Moving Checklists'} />
+                    <WhyUsText title={'01'} text={'Best Logistic Company in Town!'} />
+                    <WhyUsText title={'01'} text={'Best Moving Company In The TOWN!'} />
+                    <WhyUsText title={'01'} text={'Best Movers and packers in Town!'} />
+                </div>
             </div>
         </div>
 
--- my-app/src/page/about-us/hero-section.tsx
+++ my-app/src/page/about-us/hero-section.tsx
@@ -6,7 +6,7 @@ const HeroSection: React.FC = () => {
   return <HeroCard>
     <div className='text-start text-5xl md:text-6xl font-[1000] px-5 md:px-0'>
       <p className='text-meta-orange'>ABOUT</p>
-      <p className='mt-2 text-meta-blue'>1885 DELIVERY</p>
+      <p className='mt-2 text-meta-blue'>Horn Ok Please</p>
       <CustomUnderline styles="w-full bg-meta-orange">
         <p className='mt-2 text-meta-blue'>SERVICES</p>
       </CustomUnderline>
